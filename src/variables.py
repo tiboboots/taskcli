a8bc98a18e193ef5742aea6114a83065f9ae7fb5
Tibo <tiboboots@outlook.com>
2025-04-04 17:59:16 +0200

    Created variables.py file to store all variables

---
+++ src/variables.py
@@ -0,0 +1,9 @@
+crud_commands = ["add", "update", "delete", "list"]
+
+task_dictionary = {"id": None, 
+               "task": None, 
+               "status": None, 
+               "createdAt": None, 
+               "updatedAt": None}
+
+file_path = "userdata.json"
