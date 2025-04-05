a77d91dcc5a32f08e9c21c1fe9f14883f489dc74
Tibo <tiboboots@outlook.com>
2025-04-05 20:19:03 +0200

    Refactored executable code to use new input functions

--- src/main.py
+++ src/main.py
@@ -6,25 +6,24 @@ import functions as func
 import crud_functions as crud
 
 if __name__ == "__main__": # Only run executable code if add_tasks.py is run directly
+   while True:
     print(f'Commands are {var.crud_commands}')
     print("Enter a command followed by your task, or type 'quit' to exit the program")
     print('For example: "add" buy groceries" will add "buy groceries" as a new task')
 
-    while True:
-        func.intialize_file_if_empty() # call function to check if json file is empty
-        json_data = crud.read_json() # Get latest data from json file and save it to json_data variable
-        try:
-            user_input = input(": ").lower()
-            if user_input == 'quit': # check if user passed string of "quit" as value for input, exit program if true.
-                print("Exiting...")
-                sys.exit()
-        except KeyboardInterrupt:
-            print(" Aborting...")
-            sys.exit()
-        else:
-            parsed_command = func.parse_user_input(user_input)[1] # Parse user_input to return command to parsed_command variable
-            parsed_task = func.parse_user_input(user_input)[0]# Parse command from user_input and return string with user task, saved to parsed_input variable
-            latest_id = func.id_generator(json_data) # Generate an id based on whether json_data list is empty or not.
-            if parsed_task is not None and parsed_command == 'add': # Check to see if parsed_input is not empty, if empty then don't write data to json file.
-                crud.add_task(parsed_task, json_data, latest_id) # If not empty, then we write dictionary with new data to json file.
-                print(f'Task added successfully! Task ID is: {latest_id}')
+    func.intialize_file_if_empty() # check if json file is empty, if true then add empty list to it
+    json_data = crud.read_json() # read json file and check for new data, save data to json_data variable
+    latest_id = func.id_generator(json_data) # Generate an id for each task based on whether json file is empty or not
+    user_command = func.input_command() # ask user to specify a command, save valid command to user_command variable
+
+    if user_command == 'add':
+      user_task = func.input_task(user_command) # ask user to specify task to add, save task to user_task variable
+      crud.add_task(user_task, json_data, latest_id)# Use user_task, json_data, and latest_id variables to add task with id to file
+      print("Task successfully added!")
+
+    elif user_command == 'update':
+      task_id = func.input_id(user_command) # ask user for id if command is update, save to task_id variable
+      if task_id is not None: # check if task_id is not null, if true and it contains a valid id, then ask for task below
+        user_task = func.input_task(user_command)
+
+            
