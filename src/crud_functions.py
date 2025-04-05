57ac673c5e023a6174c788dd32c10e9b03d26d9c
Tibo <tiboboots@outlook.com>
2025-04-05 19:50:28 +0200

    Changed first add_task function parameter name to user_task

--- src/crud_functions.py
+++ src/crud_functions.py
@@ -1,17 +1,17 @@
 import variables as var
 import json
 
-def add_task(parsed_input, json_data, latest_id): 
-    var.task_dictionary["task"] = parsed_input
-    var.task_dictionary["id"] = latest_id
-    json_data.append(var.task_dictionary)
-    with open(var.file_path, "w") as json_file:
-        json.dump(json_data, json_file, indent = 4)
-
 def read_json(): # Get latest data from userdata.json file, save it to json_data variable as list
      with open(var.file_path, "r") as userdata:
         json_data = json.load(userdata)
         return json_data
+
+def add_task(user_task, json_data, latest_id): 
+    var.task_dictionary["task"] = user_task
+    var.task_dictionary["id"] = latest_id
+    json_data.append(var.task_dictionary)
+    with open(var.file_path, "w") as json_file:
+        json.dump(json_data, json_file, indent = 4)
      
 
      
