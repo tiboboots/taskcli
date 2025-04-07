9edcef62f1c2a461c5a8d4b375366835eb455561
Tibo <tiboboots@outlook.com>
2025-04-07 10:38:45 +0200

    Updated add_task function to set default task status of 'to-do'

--- src/crud_functions.py
+++ src/crud_functions.py
@@ -9,6 +9,7 @@ def read_json(): # Get latest data from userdata.json file, save it to json_data
 def add_task(user_task, json_data, latest_id): 
     var.task_dictionary["task"] = user_task
     var.task_dictionary["id"] = latest_id
+    var.task_dictionary["status"] = "to-do"
     json_data.append(var.task_dictionary)
     with open(var.file_path, "w") as json_file:
         json.dump(json_data, json_file, indent = 4)
