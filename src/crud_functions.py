d9fe09cfe614b1f57b508ca5ee11af8d46370e67
Tibo <tiboboots@outlook.com>
2025-04-09 09:32:33 +0200

    Added datetime to add_task function to specify when task was added

--- src/crud_functions.py
+++ src/crud_functions.py
@@ -1,5 +1,6 @@
 import variables as var
 import json
+from datetime import datetime
 
 def read_json(): # Get latest data from userdata.json file, save it to json_data variable as list
      with open(var.file_path, "r") as userdata:
@@ -10,6 +11,7 @@ def add_task(user_task, json_data, latest_id): # Update task_dictionary variable
     var.task_dictionary["task"] = user_task
     var.task_dictionary["id"] = latest_id
     var.task_dictionary["status"] = "to-do"
+    var.task_dictionary['createdAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     json_data.append(var.task_dictionary)
     with open(var.file_path, "w") as json_file:
         json.dump(json_data, json_file, indent = 4)
