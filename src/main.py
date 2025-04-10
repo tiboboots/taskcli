d63937a46094106f66802a3248541614eddbed28
Tibo <tiboboots@outlook.com>
2025-04-10 19:58:06 +0200

    Updated main.py file and executable code to use refactored functions

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+from datetime import datetime
 import variables as var
 import functions as func
 import crud_functions as crud
@@ -16,39 +17,10 @@ if __name__ == "__main__": # Only run executable code if add_tasks.py is run dir
     latest_id = func.id_generator(json_data) # Generate an id for each task based on whether json file is empty or not
     user_command = func.input_command() # ask user to specify a command, save valid command to user_command variable
 
-    if user_command == 'add':
-      user_task = func.input_task(user_command) # ask user to specify task to add, save task to user_task variable
-      crud.add_task(user_task, json_data, latest_id)# Use user_task, json_data, and latest_id variables to add task with id to file
-      print("Task successfully added!")
-
-    elif user_command == 'update':
-      task_id = func.input_id(user_command) # ask user for id if command is update, save to task_id variable
-      if task_id is not None: # check if task_id is not null, if true and it contains a valid id, then ask for new task below
-        user_task = func.input_task(user_command)
-        crud.update_task(task_id, json_data, user_task)
-        print("Task updated successfully!")
-
-    elif user_command == 'delete': #If command is delete, then ask for id of task to delete, and delete task if valid id is returned.
-      task_id = func.input_id(user_command)
-      if task_id is not None:
-        crud.delete_task(json_data, task_id)
-        print("Task successfully deleted!")
-
-    elif user_command == 'list': 
-      crud.list_all_tasks(json_data)
-
-    elif user_command in var.list_tasks_commands: 
-      just_the_status = func.parse_status_from_command(user_command)
-      crud.list_tasks_by_status(json_data, just_the_status)
-      '''
-      If user command is in the list_tasks_commands set, then parse the status from the command,
-      and use parsed status to return any tasks matching that status.
-      '''
-    
-    elif user_command == var.status_command: #If user command is equal to 'set', ask for task id, then a status to update a task's status.
-      task_id = func.input_id(user_command)
-      task_status = func.input_status(task_id)
-      crud.update_status(json_data, task_status, task_id)
-
-
-            
+    crud.add_task(user_command)
+    crud.update_task(user_command)
+    crud.set_status(user_command)
+    crud.delete_task(user_command)
+    crud.list_all_tasks(user_command)
+    crud.list_tasks_by_status(user_command)
+      
