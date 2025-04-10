0590e9034146a6cee67d777debef7ebe35b2161d
Tibo <tiboboots@outlook.com>
2025-04-10 16:16:37 +0200

    Created write_json function to write data to json file

--- src/crud_functions.py
+++ src/crud_functions.py
@@ -7,6 +7,10 @@ def read_json(): # Get latest data from userdata.json file, save it to json_data
      with open(var.file_path, "r") as userdata:
         json_data = json.load(userdata)
         return json_data
+     
+def write_json(json_data):
+    with open(var.file_path, "w") as json_file:
+        json.dump(json_data, json_file, indent = 4)
 
 def add_task(user_command, json_data, latest_id): # Update task_dictionary variable and it's keys to have the user specified task, generated id, and default status of 'to-do'
     if user_command == "add":
