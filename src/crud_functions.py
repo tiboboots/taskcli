d1b185fc0492a251040bd1f2b4c272564163e1ba
Tibo <tiboboots@outlook.com>
2025-04-11 10:31:00 +0200

    Refactored all crud functions to check for quit command

--- src/crud_functions.py
+++ src/crud_functions.py
@@ -29,9 +29,13 @@ def add_task(user_command): # Add new tasks to json_data list, write list back t
     return
      
 def update_task(user_command):
-    if user_command != 'update':
+    if user_command != 'update' or user_command != 'quit':
         return
-    json_data = read_json() # Get fresh data from json file
+    quit_status = func.check_if_quit(user_command)
+    if quit_status == True:
+        print("Exiting...")
+        sys.exit()
+    json_data = read_json()
     task_id = func.input_id(user_command) # ask user for id if command is update, save to task_id variable
     valid_id = func.check_id_validility(json_data, task_id)  # Check if id exists within json file's data
     if valid_id == False:
@@ -44,8 +48,12 @@ def update_task(user_command):
     return
 
 def set_status(user_command): # Update an existing task's status, if id exists and command is equal to status_command variable
-   if user_command != var.status_command:
+   if user_command != var.status_command or user_command != 'quit':
        return
+   quit_status = func.check_if_quit(user_command)
+   if quit_status == True:
+       print("Exiting...")
+       sys.exit()
    json_data = read_json()
    task_id = func.input_id(user_command)
    valid_id = func.check_id_validility(json_data, task_id)
@@ -58,8 +66,12 @@ def set_status(user_command): # Update an existing task's status, if id exists a
    print("Status updated successfully!")
 
 def delete_task(user_command): # Delete task from json file if command is equal to "delete" and id is found in json_data list
-    if user_command != 'delete':
+    if user_command != 'delete' or user_command != 'quit':
         return
+    quit_status = func.check_if_quit(user_command)
+    if quit_status == True:
+        print("Exiting...")
+        sys.exit()
     json_data = read_json()
     task_id = func.input_id(user_command)
     valid_id = func.check_id_validility(json_data, task_id)
@@ -72,8 +84,12 @@ def delete_task(user_command): # Delete task from json file if command is equal 
     return
 
 def list_all_tasks(user_command):# List all existing tasks if command equals "list" and json_data list is not empty
-    if user_command != 'list':
+    if user_command != 'list' or user_command != 'quit':
         return
+    quit_status = func.check_if_quit(user_command)
+    if quit_status == True:
+        print("Exiting...")
+        sys.exit()
     json_data = read_json()
     if len(json_data) == 0:
         return
@@ -81,8 +97,12 @@ def list_all_tasks(user_command):# List all existing tasks if command equals "li
         print(f'Task: {dictionary['task']}, Task ID: {dictionary['id']}, Task Status: {dictionary['status']}')
 
 def list_tasks_by_status(user_command): # List tasks based on user specified status
-    if user_command not in var.list_tasks_commands:
+    if user_command not in var.list_tasks_commands or user_command == 'quit':
         return
+    quit_status = func.check_if_quit(user_command)
+    if quit_status == True:
+        print("Exiting...")
+        sys.exit()
     json_data = read_json()
     just_the_status = func.parse_status_from_command(user_command)
     func.match_task_by_status(json_data, just_the_status) # Use match_task function to find and list any tasks with user specified status
