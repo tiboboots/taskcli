879588d3c4119995cb48f6f73bafa62fe4a3fdee
Tibo <tiboboots@outlook.com>
2025-04-11 17:30:42 +0200

    Removed conditional check for 'quit' input, as this is now handled invidually by all input functions

--- src/crud_functions.py
+++ src/crud_functions.py
@@ -13,10 +13,7 @@ def write_json(json_data):
     with open(var.file_path, "w") as json_file:
         json.dump(json_data, json_file, indent = 4)
 
-def add_task(user_command, quit_status): # Add new tasks to json_data list, write list back to json file with new tasks
-    if quit_status == True: # If command is quit, then use sys.exit to quit program gracefully
-        print("Exiting...")
-        sys.exit()
+def add_task(user_command): # Add new tasks to json_data list, write list back to json file with new tasks
     if user_command != 'add':
         return
     json_data = read_json() # If command is not quit, thus add, then begin add task process
@@ -27,29 +24,25 @@ def add_task(user_command, quit_status): # Add new tasks to json_data list, writ
     print("Task added successfully!")
     return
      
-def update_task(user_command, quit_status):
-    if quit_status == True:
-        print("Exiting...")
-        sys.exit()
+def update_task(user_command):
     if user_command != 'update':
         return
     json_data = read_json()
     task_id = func.input_id() # ask user for id if command is update, save to task_id variable
+    func.check_if_quit(task_id)
     valid_id = func.check_id_validility(json_data, task_id)  # Check if id exists within json file's data
     if valid_id == False:
         print("No tasks with that id exist.")
         return
     user_task = func.input_task() 
+    func.check_if_quit(user_task)
     func.updated_task_to_dictionary(json_data, task_id, user_task) # add new task to matching dictionary in json_data list
     write_json(json_data) # Write updated json_data list with new task back to json file
     print("Task successfully updated!")
     return
 
-def set_status(user_command, quit_status): # Update an existing task's status, if id exists and command is equal to status_command variable
-   if quit_status == True:
-       print("Exiting...")
-       sys.exit()
-   if user_command != 'set':
+def set_status(user_command): # Update an existing task's status, if id exists and command is equal to status_command variable
+   if user_command != var.status_command:
        return
    json_data = read_json()
    task_id = func.input_id()
@@ -62,10 +55,7 @@ def set_status(user_command, quit_status): # Update an existing task's status, i
    write_json(json_data)
    print("Status updated successfully!")
 
-def delete_task(user_command, quit_status): # Delete task from json file if command is equal to "delete" and id is found in json_data list
-    if quit_status == True:
-        print("Exiting...")
-        sys.exit()
+def delete_task(user_command): # Delete task from json file if command is equal to "delete" and id is found in json_data list
     if user_command != 'delete':
         return
     json_data = read_json()
@@ -79,10 +69,7 @@ def delete_task(user_command, quit_status): # Delete task from json file if comm
     print("Task deleted successfully!")
     return
 
-def list_all_tasks(user_command, quit_status):# List all existing tasks if command equals "list" and json_data list is not empty
-    if quit_status == True:
-        print("Exiting...")
-        sys.exit()
+def list_all_tasks(user_command):# List all existing tasks if command equals "list" and json_data list is not empty
     if user_command != 'list':
         return
     json_data = read_json()
@@ -91,10 +78,7 @@ def list_all_tasks(user_command, quit_status):# List all existing tasks if comma
     for dictionary in json_data:
         print(f'Task: {dictionary['task']}, Task ID: {dictionary['id']}, Task Status: {dictionary['status']}')
 
-def list_tasks_by_status(user_command, quit_status): # List tasks based on user specified status
-    if quit_status == True:
-        print("Exiting...")
-        sys.exit()
+def list_tasks_by_status(user_command): # List tasks based on user specified status
     if user_command not in var.list_tasks_commands:
         return
     json_data = read_json()
