19db61ccc55a1877f13fc9ca21e9f0c4cc17805f
Tibo <tiboboots@outlook.com>
2025-04-12 13:43:57 +0200

    Added 'info' command to all_commands variable

--- src/variables.py
+++ src/variables.py
@@ -16,4 +16,4 @@ different_statuses = ['to-do', 'in-progress', 'done']
 
 list_tasks_commands = {'list to-do', 'list in-progress', 'list done'}
 
-all_commands = {"add", "update", "delete", "list", "list to-do", "list in-progress", "list done", "set", "quit"}
+all_commands = {"add", "update", "delete", "list", "list to-do", "list in-progress", "list done", "set", "quit", "info"}
