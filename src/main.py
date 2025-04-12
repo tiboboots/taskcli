4860393744a331c051a0f31080e9311d1b24d98d
Tibo <tiboboots@outlook.com>
2025-04-12 15:34:54 +0200

    Removed json_data and latest_id variables and their function calls from executable code in main.py

--- src/main.py
+++ src/main.py
@@ -7,8 +7,6 @@ func.info_message()
 if __name__ == "__main__": # Only run executable code if add_tasks.py is run directly
    while True:
     func.intialize_file_if_empty() # check if json file is empty, if true then add empty list to it
-    json_data = crud.read_json() # read json file and check for new data, save data to json_data variable
-    latest_id = func.id_generator(json_data) # Generate an id for each task based on whether json file is empty or not
     user_command = func.input_command() # ask user to specify a command, save valid command to user_command variable
 
     crud.perform_task(user_command) # Use perform_task function to run the proper crud function based on user specified command
