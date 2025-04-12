00db2ac73dcd9f801e7b54596f1d1ea627afd29d
Tibo <tiboboots@outlook.com>
2025-04-12 12:14:11 +0200

    Turned list_tasks_commands variable back into set

--- src/variables.py
+++ src/variables.py
@@ -12,6 +12,6 @@ status_command = "set"
 
 different_statuses = ['to-do', 'in-progress', 'done']
 
-list_tasks_commands = ('list to-do', 'list in-progress', 'list done')
+list_tasks_commands = {'list to-do', 'list in-progress', 'list done'}
 
 all_commands = {"add", "update", "delete", "list", "list to-do", "list in-progress", "list done", "set", "quit"}
